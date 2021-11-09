e4911585acd56d10ed98d6f1e0003e7797e86141
CoelhaNegra <70537811+CoelhaNegra@users.noreply.github.com>
2021-11-09 17:07:52 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 function setup() {
   createCanvas(400,400);
-  background(51);
+  background("white");
   box = createSprite(200,200,30,30);
 
 }
@@ -12,13 +12,19 @@ function draw()
   // escreva o código para alterar a cor de fundo 
   // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
   
-  
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background("red");
 
+  
+  }
+  
   if (keyIsDown(LEFT_ARROW)) 
   {
     background("blue");
     
   }
+  
  
     if (keyIsDown(UP_ARROW)) 
   {
